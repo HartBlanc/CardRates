08e3614c0d5c05e1d868472f224371192bdad834
Callum Hart <callumhartemail@gmail.com>
2019-11-17 20:45:42 +0000

    dir cleanup

--- cancel_all.sh
+++
@@ -1,3 +0,0 @@
-for row in $(cURl 'http://localhost:6800/listjobs.json?project=CardRatesUpdater' | jq -c '.running[] | {id: .id}'); do
-   cURl 'http://localhost:6800/cancel.json?project=CardRatesUpdater' -d job=${row:7:32}
-done
