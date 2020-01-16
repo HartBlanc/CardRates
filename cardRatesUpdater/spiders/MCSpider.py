201c04d56f22dad5d6f8edcdea0fe30aba31fc65
Callum Hart <callumhartemail@gmail.com>
2020-01-16 14:16:26 +0000

    refactor: changed folder structure

---
+++ cardRatesUpdater/spiders/MCSpider.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from scrapy.utils.project import get_project_settings as settings
+from ..items import UpdaterItem
+import scrapy
+
+from datetime import datetime
+
+from pathlib import Path
+import csv
+
+import requests
+import json
+
+std_date_fmt = settings().get('STD_DATE_FMT')
+
+
+class MCSpider(scrapy.Spider):
+    # Need name to call spider from terminal
+    name = 'MCSpider'
+    provider = 'Mastercard'
+    allowed_domains = ['mastercard.co.uk']
+
+    url = 'https://www.mastercard.co.uk/'
+    curr_url = url + 'settlement/currencyrate/settlement-currencies'
+    support_url = url + 'en-gb/consumers/get-support/convert-currency.html'
+    rate_url = url + 'settlement/currencyrate/{}/conversion-rate'
+    date_fmt = '%Y-%m-%d'
+
+    # err_msgs to be identified.
+    # err_msgs = {'101': None,
+    #             '104': None,
+    #             '114': "Not Found , Conversion rate is not available for this currency pair.", todo should this add null to databse?
+    #             '400': None,
+    #             '401': None,
+    #             '500': None,
+    #             }
+
+
+    rate_params = {'fxDate': None, 'transCurr': None, 'crdhldBillCurr': None,
+                   'bankFee': '0.0', 'transAmt': '1'}
+
+    def __init__(self, data=None, in_path=None, *args, **kwargs):
+        super(MCSpider, self).__init__(*args, **kwargs)
+        self.in_path = Path(in_path)
+
+    # a generator function for initial requests
+    # (formatted urls from currency alphaCds and dates)
+    def start_requests(self):
+        with self.in_path.open() as data:
+            for card_c, trans_c, date in csv.reader(data):
+                item = UpdaterItem(card_c, trans_c, date)
+
+                params = dict(self.rate_params)
+                params['crdhldBillCurr'] = card_c
+                params['transCurr'] = trans_c
+                params['fxDate'] = self.fmt_date(date)
+
+                param_string = ''.join(f'{k}={v};' for k, v in params.items())[:-1]
+
+                yield (scrapy.Request(
+                    url=self.rate_url.format(param_string),
+                    headers={'referer': self.support_url},
+                    meta=dict(item=item)))
+
+    def parse(self, response):
+        item = response.meta['item']
+
+        j_response = json.loads(response.body_as_unicode())
+        if 'errorCode' in j_response['data']:
+            err_cd = j_response['data']['errorCode']
+            print(f"Dropping Item: {item}, Error msg: \"{j_response['data'].get('errorMessage')}\"")
+            return
+        else:
+            item['rate'] = j_response['data']['conversionRate']
+
+        wanted = {'card_c': None, 'trans_c': None, 'date': None, 'rate': None}
+        unwanted_keys = set(item.keys()) - set(wanted.keys())
+        for unwanted_key in unwanted_keys:
+            item.pop(unwanted_key, None)
+        return item
+
+    @classmethod
+    def fetch_avail_currs(cls):
+        r = requests.get(cls.curr_url, headers={"referer": cls.support_url})
+
+        assert r.ok, "Request failed - ip may be blocked"
+
+        codes = {x['alphaCd']: x['currNam'].strip()
+                 for x in r.json()['data']['currencies']}
+
+        assert len(codes) != 0, "No currencies found, check url and selector"
+
+        return codes
+
+    @classmethod
+    def fmt_date(cls, std_date):
+        return (datetime.strptime(std_date, std_date_fmt)
+                .strftime(cls.date_fmt))
--- tests/test_db_client.py
+++ tests/test_db_client.py
@@ -7,7 +7,7 @@ from os import environ
 import pytest
 
 # Hacky solution to import errors...
-sys.path[0] = str(Path(sys.path[0]) / "src")
+sys.path[0] = str(Path(sys.path[0]) / "cardRatesUpdater")
 
 TEST_DATE = date(day=10, month=9, year=1995)
 
@@ -15,7 +15,7 @@ TEST_DATE = date(day=10, month=9, year=1995)
 @pytest.fixture()
 def db_client():
     # set up
-    from src.db_client import DbClient
+    from db.client import DbClient
     db_url = environ.get("DB_URL")
     name_start = db_url.rfind('/') + 1
     db_url = f"{db_url[:name_start]}Test"
@@ -32,13 +32,13 @@ def db_client():
 
 
 def test_strpdate():
-    from src.db_client import strpdate
+    from db.client import strpdate
     assert strpdate("10/09/1995") == TEST_DATE
     assert strpdate("09/10/1995", "%m/%d/%Y") == TEST_DATE
 
 
 def test_current_date():
-    from src.db_client import DbClient
+    from db.client import DbClient
     assert DbClient.current_date() is not None
 
 
@@ -46,7 +46,7 @@ def test_create_tables(db_client):
     tables = db_client.engine.table_names()
     assert set(tables) == {"providers", "currency_codes", "rates"}
 
-    from src.db_orm import Provider, CurrencyCode
+    from db.orm import Provider, CurrencyCode
     with db_client.session_scope(commit=False) as s:
         assert set(s.query(Provider.name)) == {(spider.provider,) for spider in db_client.spiders}
 
